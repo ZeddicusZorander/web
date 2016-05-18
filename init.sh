e31c86593728ef23545132a2f00b71412a7db69b
ZeddicusZorander <zeddicus.zul.zorander@ya.ru>
2016-05-19 02:05:16 +0300

    update config files

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ mv etc/ ~/
 sudo ln -sf ~/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf ~/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-gunicorn -c /etc/gunicorn.d/hello.py hello:app
+gunicorn -b 0.0.0.0:8080 hello:AppModule
