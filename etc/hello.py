e1c5c012ea1159ac2fa22b1ea4ed67d193bbabe3
ZeddicusZorander <zeddicus.zul.zorander@ya.ru>
2016-05-19 01:06:40 +0300

    update app module

--- etc/hello.py
+++ etc/hello.py
@@ -1,2 +1,10 @@
-bind = '0.0.0.0:8000'
-workers = 1
+CONFIG = {
+    'mode': 'wsgi',
+    'working_dir': '/home/box/web/',
+    'args': (
+        '--bind=127.0.0.1:8000',
+        '--workers=16',
+        '--timeout=60',
+        'AppModule.module',
+    ),
+}
--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
-def app(environ, start_response):
-    data = "\n".join(environ['QUERY_STRING'].split('&')).encode()
-    response_headers = [
-        ('Content-type','text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
-    start_response('200 OK', response_headers)
-    return [data]
+class AppModule:
+
+    def __init__(self, environ, start_response):
+        self.environ = environ
+        self.start = start_response
+
+    def __iter__(self):
+    	data = "\n".join(self.environ['QUERY_STRING'].split('&')).encode()
+    	response_headers = [
+	        ('Content-type','text/plain'),
+	        ('Content-Length', str(len(data)))
+	    ]
+    	self.start('200 OK', response_headers)
+        yield data
