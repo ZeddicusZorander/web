cbf43e53342fced303d0b43a0ce9313719cef21a
ZeddicusZorander <zeddicus.zul.zorander@ya.ru>
2016-05-19 16:02:44 +0300

    update config files

--- etc/hello.py
+++ etc/hello.py
@@ -2,9 +2,9 @@ CONFIG = {
     'mode': 'wsgi',
     'working_dir': '/home/box/web/',
     'args': (
-        '--bind=0.0.0.0:8080',
-        '--workers=16',
+        '--bind=0.0.0.0:8000',
+        '--workers=4',
         '--timeout=60',
-        'AppModule.module',
-    ),
+        'hello:AppModule'
+    )
 }
--- init.sh
+++ init.sh
@@ -3,4 +3,3 @@ mv etc/ ~/
 sudo ln -sf ~/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf ~/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 hello:AppModule
